a32df7d54531dd25a089e517ed96a45adb023d5c
MinseongKim <autumn311@snu.ac.kr>
2024-06-12 14:50:54 +0000

    Implemented main.cpp file

--- main.cpp
+++ main.cpp
@@ -1,22 +1,36 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
+#include<ctime>
 
 using namespace std;
 
-int N,M;
+int N, M;
 vector<int> vec;
 
-void func(int cnt) {
-
-
-
-
-
-
-
+void func(int cnt, int n) {
+	if (vec.size() == cnt) {
+		for (int i = 0; i < cnt; i++) {
+			printf("%d ", vec[i] + 1);
+			// cout << vec[i]+1 << ' ';
+		}
+		printf("\n");
+		// cout << time(NULL) << endl;
+	}
+	else {
+		for (int i = 0; i < n; i++) {
+			if (find(vec.begin(), vec.end(), i) == vec.end()) {
+				vec.push_back(i);
+				func(cnt, n);
+				vec.pop_back();
+			}
+		}
+	}
 }
 
 int main() {
-   cin >> N >> M;
-   func(0);
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cin >> N >> M;
+	func(M, N);
 }
